350d6eaff2fee06655d7f62151ca36e18a2ef6cf
Ricardo Calderon <ricardocalderon@apoioprodesp.sp.gov.br>
2022-11-15 15:57:55 -0300

    updates

--- assets/dist/js/main.js
+++ assets/dist/js/main.js
@@ -10,4 +10,26 @@ document.addEventListener('DOMContentLoaded', (event) => {
         })
 
     };
+    let form = document.querySelector('form');
+    let rsvp = form.querySelector('input[name="rsvp"]');
+
+    rsvp.addEventListener('change', (event) =>{
+        if (rsvp.value == 'sim'){
+            document.querySelector('#rsvp').classList.add('d-none');
+            document.querySelector('#info').classList.remove('d-none');            
+        }
+    })
+
+    form.onsubmit = async (e) => {
+        e.preventDefault();
+    
+        let response = await fetch('https://nate-api.vercel.app/api/posts/', {
+          method: 'POST',
+          body: new FormData(form)
+        });
+    
+        let result = await response.json();
+    
+        alert(result.message);
+    };
 })
